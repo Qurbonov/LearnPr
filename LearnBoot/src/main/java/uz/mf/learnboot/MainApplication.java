2b293ba081d94c0a9d296fe07ca7b334f9c93dee
qurbonov <qurbonov.p@gmail.com>
2015-09-09 18:16:38 +0500

    add files

---
+++ LearnBoot/src/main/java/uz/mf/learnboot/MainApplication.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uz.mf.learnboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author qurbonov
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class MainApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+ 
+}
